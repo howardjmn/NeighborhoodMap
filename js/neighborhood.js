00021197b4ccfcee3a25b42f03c3b2598e3ae796
howardjmn <semiles@msn.com>
2017-07-06 18:59:48 -0500

    added markers and infowindows

--- js/neighborhood.js
+++ js/neighborhood.js
@@ -2,50 +2,37 @@ var locations =
 [
   { name: 'Surly Brewing',
     address: '520 Malcolm Ave SE, Minneapolis, MN 55414',
-    latitude: 44.973279,
-    longitude: -93.209802,
-    website: 'http://surlybrewing.com/',
-    marker: ''
-  },
-  { name: 'Lake Monster Brewing',
-    address: '550 Vandalia St #160, St Paul, MN 55114',
-    latitude: 44.957913,
-    longitude: -93.191392,
+    location: {lat: 44.973279, lng: -93.209802},
     website: 'http://www.lakemonsterbrewing.com/',
     marker: ''
   },
   { name: 'Urban Growler Brewing',
     address: '2325 Endicott St, St Paul, MN 55114',
-    latitude: 44.970632,
-    longitude: -93.193282,
+    location: {lat: 44.970632, lng: -93.193282},
     website: 'http://www.urbangrowlerbrewing.com/',
     marker: ''
   },
   { name: 'BlackStack Brewing',
     address: '755 Prior Ave N, St Paul, MN 55104',
-    latitude: 44.964203,
-    longitude: -93.182941,
+    location: {lat: 44.964203, lng: -93.182941},
     website: 'http://www.blackstackbrewing.com/',
     marker: ''
   },
   { name: 'Fulton Brewery',
     address: '414 N 6th Ave, Minneapolis, MN 55401',
-    latitude: 44.985134,
-    longitude: -93.279214,
+    location: {lat: 44.985134, lng: -93.279214},
     website: 'http://www.fultonbeer.com/',
     marker: ''
   },
   { name: 'Eastlake Craft Brewery',
     address: '920 E Lake St, Minneapolis, MN 55407',
-    latitude: 44.948903,
-    longitude: -93.260741,
+    location: {lat: 44.948903, lng: -93.260741},
     website: 'http://www.eastlakemgm.com/',
     marker: ''
   },
   { name: 'Wild Mind Artisan Ales',
     address: '6031 Pillsbury Ave S, Minneapolis, MN 55419',
-    latitude: 44.893291,
-    longitude: -93.281513,
+    location: {lat: 44.893291, lng: -93.281513},
     website: 'http://www.wildmindales.com/',
     marker: ''
   }
@@ -55,8 +42,8 @@ var NeighborhoodPlace = function(data)
 {
   this.name = ko.observable(data.name);
   this.address = ko.observable(data.address);
-  this.latitude = ko.observable(data.latitude);
-  this.longitude = ko.observable(data.longitude);
+  this.latitude = ko.observable(data.location.lat);
+  this.longitude = ko.observable(data.location.lng);
   this.website = ko.observable(data.website);
   this.marker = ko.observable(data.marker);
 };
@@ -68,6 +55,15 @@ function initMap()
     //Self alias
     var self = this;
 
+    //Center map on University of Minnesota east bank
+    this.map = new google.maps.Map(document.getElementById('map'),
+    {
+      zoom: 12,
+      center: {lat: 44.978409, lng: -93.234671}
+    });
+    this.markers = [];
+    this.infoWindow = new google.maps.InfoWindow();
+
     // create list of neighborhood locations
     this.neighborhoodPlaces = ko.observableArray([]);
 
@@ -76,17 +72,54 @@ function initMap()
         self.neighborhoodPlaces.push(new NeighborhoodPlace(neighborhoodItem));
     });
 
-    //Center map on University of Minnesota east bank
-    var map = new google.maps.Map(document.getElementById('map'),
-    {
-      zoom: 12,
-      center: {lat: 44.978409, lng: -93.234671}
-    });
-
     // Link marker to selected list item
     self.selectedMarker = function(marker)
     {
       google.maps.event.trigger(this.marker, 'click');
+    };
+
+    // The following loop uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++)
+    {
+      // Get the position from the location array.
+      var position = locations[i].location;
+      var name = locations[i].name;
+      var address = locations[i].address;
+
+      // Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker
+      ({
+          position: position,
+          name: name,
+          address: address,
+          animation: google.maps.Animation.DROP,
+          id: i
+      });
+
+      // Create an onclick event to open an infoWindow   at each marker.
+      marker.addListener('click', function()
+      {
+          // Check to make sure the infoWindow   is not already opened on this marker.
+          if (self.infoWindow.marker != this)
+          {
+              self.infoWindow.marker = this;
+              self.infoWindow.setContent
+                ('<div>' + this.name + '</div>' +
+                 '<div>' + this.address + '</div>');
+              self.infoWindow.open(self.map, this);
+              // Make sure the marker property is cleared if the infoWindow   is closed.
+              self.infoWindow.addListener('closeclick', function()
+              {
+                self.infoWindow.marker = null;
+              });
+          }
+       });
+
+      //show marker
+      marker.setMap(self.map);
+
+      // Push the marker to our array of markers.
+      self.markers.push(marker);
     };
   };
 
